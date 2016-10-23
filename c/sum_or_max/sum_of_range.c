a3f55bef2816556c6d7db86c3a3ecc64d2d1988d
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-10-23 23:26:17 +0900

    Add sum_of_range.c

---
+++ c/sum_or_max/sum_of_range.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/*
+
+Σ[m→n]k
+= Σ[0→n]k - Σ[0→m-1]k
+= n(n+1)/2 - m(m-1)/2
+= {n(n+1) - m(m-1)} / 2
+
+*/
+
+int sumOfRange(int m, int n)
+{
+    if (m > n) {
+        m ^= n;
+        n ^= m;
+        m ^= n;
+    }
+    return (n * (n + 1) - m * (m - 1)) / 2;
+}
+
+int main(void)
+{
+    int m, n;
+
+    printf("m = ");
+    if (scanf("%d", &m) != 1) {
+        return 1;
+    }
+
+    printf("n = ");
+    if (scanf("%d", &n) != 1) {
+        return 1;
+    }
+
+    printf("sumOfRange(%d, %d) = %d\n", m, n, sumOfRange(m, n));
+    return 0;
+}
