0d5e1a9fbb7c80b547c76e3af8700ca63ae00297
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-10-24 00:25:59 +0900

    Bash依存していた

--- sh/join/join-sorted.sh
+++ sh/join/join-sorted.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
 join -o '1.2 1.3 1.4 0 1.2' <(sort t1.txt) <(sort t2.txt) | sort -k 1 | sort -nk 2
--- sh/join/join.sh
+++ sh/join/join.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
 join -o '1.2 1.3 1.4 0 2.2' <(sort t1.txt) <(sort t2.txt)
